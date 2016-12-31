c97611a61d7d1cae15c9cbce2532cd362411137f
Zhiquan Yeo <zyeo8@bloomberg.net>
2016-12-30 22:44:18 -0500

    Initial commit of core notifications module

---
+++ src/common/Notifications.js
@@ -0,0 +1,8 @@
+import { Position, Toaster } from '@blueprintjs/core';
+
+const Notifications = Toaster.create({
+    className: 'ftl-notifications',
+    position: Position.TOP
+});
+
+export default Notifications;
