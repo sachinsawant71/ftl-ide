2d2cb26a2b8e63e728c33531599e5aebb0073103
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2017-01-14 20:30:49 -0500

    Allow FileExplorer to display context menu for items

--- src/utils/FileStructureUtils.js
+++ src/utils/FileStructureUtils.js
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FileStructureTypes } from '../Constants';
-import { Tooltip } from '@blueprintjs/core';
+import { Tooltip, ContextMenuTarget, Menu, MenuItem, MenuDivider } from '@blueprintjs/core';
 
-function makeFolder(def, path) {
+class FileStructureLabel extends Component {
+    handleContextMenu(type, key) {
+        console.log('Handling action "' + type + '" for ', key);
+        if (this.props.handlers) {
+            if (this.props.handlers[type]) {
+                this.props.handlers[type](key);
+            }
+        }
+    }
+
+    renderContextMenu() {
+        if (this.props.type === FileStructureTypes.FOLDER) {
+            return (
+                <Menu>
+                    <MenuItem onClick={this.handleContextMenu.bind(this, 'addFile', this.props.labelKey)} text="Add File"/>
+                    <MenuItem onClick={this.handleContextMenu.bind(this, 'addFolder', this.props.labelKey)} text="Add Folder"/>
+                    <MenuDivider/>
+                    <MenuItem onClick={this.handleContextMenu.bind(this, 'deleteFolder', this.props.labelKey)} text="Delete Folder"/>
+                </Menu>
+            )
+        }
+        else {
+            return (
+                <Menu>
+                    <MenuItem onClick={this.handleContextMenu.bind(this, 'deleteFolder', this.props.labelKey)} text="Delete File"/>
+                </Menu>
+            )
+        }
+    }
+
+    render() {
+        return <div>{this.props.displayText}</div>;
+    }
+}
+
+ContextMenuTarget(FileStructureLabel);
+
+function makeFolder(def, path, handlers) {
+    var folderLabel = (
+        <Tooltip content={def.label}>
+            <FileStructureLabel labelKey={path + '/' + def.label} displayText={def.label} type={FileStructureTypes.FOLDER} handlers={handlers}/>
+        </Tooltip>
+    );
+
     var ret = {
         iconName: def.isExpanded ? 'folder-open' : 'folder-close',
         isExpanded: !!def.isExpanded,
         hasCaret: true,
-        label: <Tooltip content={def.label}>{def.label}</Tooltip>,
+        label: folderLabel,
         key: path + '/' + def.label,
         type: FileStructureTypes.FOLDER,
         childNodes: []
@@ -28,24 +71,30 @@ function makeFolder(def, path) {
     return ret;
 }
 
-function makeItem(def, path) {
+function makeItem(def, path, handlers) {
+    var itemLabel = (
+        <Tooltip content={def.label}>
+            <FileStructureLabel labelKey={path + '/' + def.label} displayText={def.label} type={FileStructureTypes.ITEM} handlers={handlers}/>
+        </Tooltip>
+    );
+
     return {
         iconName: 'document',
-        label: <Tooltip content={def.label}>{def.label}</Tooltip>,
+        label: itemLabel,
         key: path + '/' + def.label,
         type: FileStructureTypes.ITEM
     }
 }
 
-function generateTreeNodes(rootList) {
+function generateTreeNodes(rootList, handlers) {
     var ret = [];
     for (var i = 0; i < rootList.length; i++) {
         var currItem = rootList[i];
         if (currItem.type === FileStructureTypes.FOLDER) {
-            ret.push(makeFolder(currItem, ''));
+            ret.push(makeFolder(currItem, '', handlers));
         }
         else if (currItem.type === FileStructureTypes.ITEM) {
-            ret.push(makeItem(currItem, ''));
+            ret.push(makeItem(currItem, '', handlers));
         }
     }
 
